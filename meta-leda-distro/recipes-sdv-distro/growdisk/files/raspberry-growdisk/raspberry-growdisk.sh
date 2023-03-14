a65aca817dfa315155bd82a02aebd5b9ac7021bc
Stoyan Lachev <stoyan.lachev@bosch.io>
2023-03-14 15:05:36 +0200

    Update raspberry-growdisk.sh

--- meta-leda-distro/recipes-sdv-distro/growdisk/files/raspberry-growdisk/raspberry-growdisk.sh
+++ meta-leda-distro/recipes-sdv-distro/growdisk/files/raspberry-growdisk/raspberry-growdisk.sh
@@ -12,7 +12,6 @@
 # * SPDX-License-Identifier: EPL-2.0
 # ********************************************************************************/
 #
-
 echo "Enlarging SD Card partition"
 
 DEVICE=$(lsblk -l | grep part | tail -1 | awk '{print $1}' | sed 's/..$//')
